2509f25cfa0ec4d3ef6eea24d5f7c324336decfd
Iwan Rahman <1w3n.rahman@gmail.com>
2023-08-06 01:28:36 -0400

    Make header responsive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import './style.css';
-import {hero, menu} from './hero.js';
+import {hero, menu as nav} from './hero.js';
 import about from './about.js';
 
+
+let menu = nav();
 document.body.appendChild(hero());
-document.body.appendChild(menu());
+document.body.appendChild(menu);
+
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 350){
+    menu.classList.add("small-nav");
+    console.log("TEST");
+  }else{
+    menu.classList.remove("small-nav");
+    console.log("END TEST");
+  }
+}
+
 document.body.appendChild(about());
 alert("Hello Webpack");
